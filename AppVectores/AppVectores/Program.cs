e7b619ba0fbadd26f9ca621a5b78f3b8d9efb9bb
Adrian Fernandez <alanfer2606@gmail.com>
2018-11-22 09:27:00 -0500

    Bloque try catch

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -14,7 +14,37 @@ namespace AppVectores
             Console.WriteLine("Control de Versiones2");
             Console.WriteLine("Control de Versiones3");
             Console.WriteLine("Control de Versiones4");
+            //*********************************************************************************************
+            Console.WriteLine("3 elevado a la 4 es igual a: {0}", potencia(3,4));
+            try
+            {
+                Console.WriteLine("-5 elevado a la 2 es igual a: {0}", potencia(-5, 2));
+            } catch(Exception ex)
+            {
+                Console.WriteLine("\nError: " + ex.Message.ToString());
+            }
+            //*********************************************************************************************
+            Console.WriteLine("\n 2 elevado a la 5 es igual a: {0}", potencia(2, 5));
             Console.ReadKey();
+
+        }
+
+        public static double potencia (double x, double y)
+        {
+            if (x<=0)
+            {
+                throw new Exception("X debe ser positivo..");
+            }
+            if (y<=0)
+            {
+                throw new Exception("Y debe ser positivo...");
+            }
+            double p = 1;
+            for (int i=1; i<=y;i++)
+            {
+                p = p * x;
+            }
+            return p;
         }
     }
 }
